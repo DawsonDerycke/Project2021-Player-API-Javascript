ebacf3e478cbd464242e3c36e44af032467e8672
DawsonDerycke <dawsonderycke@hotmail.com>
2021-03-13 15:48:19 +0100

    deploiement heroku

--- bdd.js
+++ bdd.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: '51.91.208.24',
+    host: '135.125.95.178',
     user: 'dawson',
-    password: 'dawson5689', 
+    password: '22j28m=PmT+4e+?<', 
     database: 'joueur_projet'
 });
 
